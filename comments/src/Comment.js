add4cdc2162c2dd97f6d1aa1b2a2008e99ec8b47
Bruno Sabino <bruno.p.sabino@gmail.com>
2019-07-25 00:09:09 -0300

    Testes unitários com Enzyme

--- comments/src/Comment.js
+++ comments/src/Comment.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const Comment = ({c}) => {
-
+    //Condicional para testes
+    let comment = 'vazio'
+    if(c && c.comment){
+        comment = c.comment
+    }
     return(
-        <div>Comentário: {c.comment}</div>
+        <div>Comentário: {comment}</div>
     )
 }
 
