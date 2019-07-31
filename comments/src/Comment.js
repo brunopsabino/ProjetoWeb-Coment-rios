b2e412dccdd1b44d2ba341cef03e441ad902cd98
Bruno Sabino <bruno.p.sabino@gmail.com>
2019-07-31 00:08:34 -0300

    Ajuste de layout

--- comments/src/Comment.js
+++ comments/src/Comment.js
@@ -7,12 +7,18 @@ const Comment = ({c}) => {
     if(c && c.comment){
         comment = c.comment
     }
+
     return(
-        <div className="_Comment">
-            <div className="_Comment_Component">
-                <b>Comentário:</b> {comment}
+        
+            <div className="row">
+                <div className="col-sm-10 col-md-8 col-lg-10 _Comment_Component">
+                    <b>Comentário:</b> {comment}
+                </div>
+                <div className="col-sm-2 col-md-4 col-lg-2 _Comment_Component">
+                    <button className="btn btn-danger btn-sm">Excluir</button>
+                </div>
             </div>
-        </div>
+        
     )
 }
 
--- comments/src/Comments.js
+++ comments/src/Comments.js
@@ -3,12 +3,12 @@ import React from 'react'
 import Comment from './Comment'
 
 const Comments = ({comments}) => {
-
     const keys = Object.keys(comments)
     return(
-        <div>
-            {keys.map(key => <Comment key={key} c={comments[key]} />)}
+        <div className="container-fluid">
+            {keys.map(key => <Comment key={key} c={comments[key] }/>)}
         </div>
+        
     )
 }
 
